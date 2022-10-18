abfef8839d4ca67cfb8e8355f2d859b23d168c0b
hyunyonglee <hyunyong.rhee@gmail.com>
2022-10-18 16:12:05 +0900

    revise

--- run_model.py
+++ run_model.py
@@ -193,7 +193,7 @@ dmrg_params = {
     # 'mixer': True,  # setting this to True helps to escape local minima
     'mixer' : dmrg.SubspaceExpansion,
     'mixer_params': {
-        'amplitude': 1.e-5,
+        'amplitude': 1.e-2,
         'decay': 1.0,
         'disable_after': disable_after
     },
