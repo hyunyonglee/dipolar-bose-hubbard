804ba9f204265386383486ad4cc86956a63e1f1c
hyunyonglee <hyunyong.rhee@gmail.com>
2022-10-18 16:14:00 +0900

    revise

--- run_model.py
+++ run_model.py
@@ -194,7 +194,7 @@ dmrg_params = {
     'mixer' : dmrg.SubspaceExpansion,
     'mixer_params': {
         'amplitude': 1.e-2,
-        'decay': 1.0,
+        'decay': 3.0,
         'disable_after': disable_after
     },
     'trunc_params': {
